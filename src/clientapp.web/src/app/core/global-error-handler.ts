e86243dbd7471fc743fd2f49f7268e46c8ec3efc
Alex Titarenko <alx.titarenko@gmail.com>
2021-07-21 08:16:48 -0700

    code refactoring

--- src/clientapp.web/src/app/core/global-error-handler.ts
+++ src/clientapp.web/src/app/core/global-error-handler.ts
@@ -3,17 +3,12 @@ import { AnalyticsService } from './analytics/analytics.service';
 
 @Injectable()
 export class GlobalErrorHandler extends ErrorHandler {
-  constructor(private injector: Injector) {
+  constructor(private readonly analyticsService: AnalyticsService) {
     super();
   }
 
   handleError(error: any): void {
     super.handleError(error);
-
-    const analyticsService = this.injector.get(AnalyticsService);
-
-    if (analyticsService) {
-      analyticsService.trackException(error.stack, true);
-    }
+    this.analyticsService.trackException(error.stack, true);
   }
 }
