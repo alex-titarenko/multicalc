e90bd7187d961c8e9bbb3a8752ae013758fff19c
Alex Titarenko <alx.titarenko@gmail.com>
2023-11-01 20:47:10 -0700

    remove artifical delay when open insert function dialog

--- src/app/calculator/insert-function/insert-function.component.ts
+++ src/app/calculator/insert-function/insert-function.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Inject, HostListener, EventEmitter, Output, AfterViewInit } from '@angular/core';
-import { transition, style, animate, trigger, state } from '@angular/animations';
+import { Component, OnInit, Inject, HostListener, EventEmitter, Output } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { FunctionMetadata } from 'mathcore/expressions/trees/metadata/function-metadata';
@@ -12,19 +11,11 @@ interface FunctionGroup {
 @Component({
   selector: 'app-insert-function',
   templateUrl: './insert-function.component.html',
-  styleUrls: ['./insert-function.component.scss'],
-  animations: [
-    trigger('openDialog', [
-      state('false', style({ opacity: 0 })),
-      state('true', style({ opacity: 1 })),
-      transition('* => true', animate('1s ease-out'))
-    ])
-  ]
+  styleUrls: ['./insert-function.component.scss']
 })
 export class InsertFunctionComponent implements OnInit {
   public functionGroups: FunctionGroup[];
   public gridColumns: number = 1;
-  public isOpen = false;
 
   @Output()
   public insertFunction = new EventEmitter<string>();
@@ -52,7 +43,6 @@ export class InsertFunctionComponent implements OnInit {
 
   ngOnInit() {
     this.updateGridColumns();
-    setTimeout(() => { this.isOpen = true; }, 400);
   }
 
   public cancel(): void {
